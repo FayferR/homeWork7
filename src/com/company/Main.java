e220bed2be31becfe8bbd77060d6a2ef54078bac
FayferR <fayfer07@mail.ru>
2022-04-17 19:19:04 +0600

    Изменил код

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Hero[] heroes = {new Medic(),new Warrior(),new Magic()};
+        Hero[] heroes = {new Medic(), new Warrior(), new Magic()};
         for (int i = 0; i < heroes.length; i++) {
             applySuperAbilities(heroes[i]);
-            if(heroes[i] instanceof Hero){
-                ((Hero)heroes[i]).applySuperAbility();
-            }
+            ((Hero) heroes[i]).applySuperAbility();
         }
     }
+
     public static void applySuperAbilities(HavingSuperAbility heroes) {
     }
 }
